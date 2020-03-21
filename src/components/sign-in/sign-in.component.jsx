b4be58dde6689b0c4c9fb15654481476a75bc1ae
redass12 <redataraoui1@gmail.com>
2020-03-21 13:01:06 +0100

    adding sign-in component inside the sign-in-and-sign-up page as well as custom form input and button inside the sign-in component

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,72 @@
+import React from 'react';
+
+import './sign-in.style.scss';
+
+import FormInput from '../form-input/form-input.component';
+
+import CustomButton from '../custom-button/custom-button.component';
+
+class SignIn extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password:''
+        }
+
+    }
+
+    handleChange = event => {
+        const {value , name} = event.target;
+        this.setState({[name]:value})
+        console.log(this.state.email);
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+
+        this.setState({email:'' , password:''});
+
+    }
+
+    render() {
+        return (
+
+            <div className='sing-in'>
+
+                <h2>I already have an account</h2>
+                <span>Sign in with your email and password</span>
+
+                <form onSubmit={this.handleSubmit}>
+                
+                    <FormInput name="email"
+                            type='email' 
+                            value={this.state.email} 
+                            required
+                            handleChange={this.handleChange}
+                            label='email'
+                            />
+
+                    <FormInput name="password"
+                            type='password' 
+                            value={this.state.password} 
+                            required
+                            handleChange={this.handleChange}
+                            label='password'
+                            />
+
+                    <CustomButton type='submit'>
+                        Submit Form
+                    </CustomButton>
+                
+                </form>
+
+            </div>
+        )
+
+    }
+
+} 
+
+export default SignIn;
